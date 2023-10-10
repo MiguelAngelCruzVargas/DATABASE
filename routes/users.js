bb411f813ace549f62461a99bf0920fa6babc709
MiguelAngelCruzVargas <144273695+MiguelAngelCruzVargas@users.noreply.github.com>
2023-10-09 19:40:35 -0600

    Add files via upload
    primera conexion a una base de datos

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,12 @@ const {listUsers} = require('../controllers/users');
 
 const router = Router();
 
+
 //http://localhost:3000/api/v1/users/
 router.get('/', listUsers);
+//router.post('/', listUsers);
+//router.put('/', listUsers);
+//router.patch('/', listUsers);
+//router.delete('/', listUsers);
 
 module.exports = router;
