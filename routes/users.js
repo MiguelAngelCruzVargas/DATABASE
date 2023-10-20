ae1512ffe6962908e7932408dd6548f967d2e8f5
MiguelAngelCruzVargas <144273695+MiguelAngelCruzVargas@users.noreply.github.com>
2023-10-19 18:20:13 -0600

    endpoint route for deleting

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,22 @@
-const {Router} = require('express');
-const {listUsers} = require('../controllers/users');
+const { Router } = require('express')
+
+const { listUsers, 
+    listUserByID, 
+    addUser,
+    deleteUser,
+   
+} = require('../controllers/users')
 
 const router = Router();
 
 
 //http://localhost:3000/api/v1/users/
-router.get('/', listUsers);
-//router.post('/', listUsers);
-//router.put('/', listUsers);
-//router.patch('/', listUsers);
-//router.delete('/', listUsers);
 
+router.get('/', listUsers); //obtiene la lista de  los usuarios en la  BD
+router.get('/:id', listUserByID); // obtiene la lista de  los usuarios en la  BD por ID
+//router.post('/', listUsers);
+router.put('/', addUser); //Agrega un nuevo usuario
+//router.put('/id', updateUser);
+//router.patch('/:id', updateUser);//  Actualiza un usuario por su ID
+router.delete('/:id', deleteUser);
 module.exports = router;
