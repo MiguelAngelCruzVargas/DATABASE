c7aac7b35c58773ebd2f01d9c57eb3b91d82d179
MiguelAngelCruzVargas <144273695+MiguelAngelCruzVargas@users.noreply.github.com>
2023-10-19 18:23:33 -0600

    endpoint route for deleting

--- models/users.js
+++ models/users.js
@@ -1,11 +1,67 @@
 
-const usersModel = {
-getAll: `SELECT 
- * 
- FROM 
-   Users `,
-
+const usermodels = {
+    getAll: `
+    SELECT 
+    * 
+    FROM 
+    Users`,
+getByID:`
+    SELECT
+    *
+    FROM
+     Users
+    WHERE
+      id = ?
+`,
+addRow: `
+    INSERT INTO
+    Users (
+     username,
+     email,
+     password,
+     name,
+     lastname,
+     phone_number,
+     role_id,
+     id_isactive
+    ) 
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+`,
+getByUsername: `
+    SELECT 
+        id 
+    FROM
+        Users
+    WHERE
+        username = ?
+        `,
+getByEmail:`
+    SELECT
+        id
+    FROM
+        Users
+    WHERE
+        email = ? `,
+       
+        updateUser: `
+        UPDATE Users
+        SET email = ?,
+            password = ?,
+            name = ?,
+            lastname = ?,
+            phone_number = ?,
+            role_id = ?,
+            id_isactive = ?
+        WHERE username = ?
+    `,
+    deleteRow:`
+    UPDATE 
+        Users
+    SET 
+        id_isactive = 0
+    WHERE 
+        id=?`,
 
 }
 
-module.exports = usersModel;
+module.exports = usermodels;
